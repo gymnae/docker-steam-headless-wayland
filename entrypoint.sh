fd2befdafd2ecd5883840ff1fedde9a54c545a32
Gunnar <git@grundstil.de>
2025-12-05 09:53:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,9 +79,6 @@ su - steam -c "export XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR && \
 
 # --- 6. Gamescope ---
 echo "Starting Gamescope..."
-# DUALSENSE MAPPING FIX
-export SDL_GAMECONTROLLERCONFIG="050000004c050000c405000000850000,PS5 Controller,a:b0,b:b1,back:b8,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b10,leftshoulder:b4,leftstick:b11,lefttrigger:a2,leftx:a0,lefty:a1,rightshoulder:b5,rightstick:b12,righttrigger:a5,rightx:a3,righty:a4,start:b9,x:b2,y:b3,platform:Linux,"
-
 sudo -E -u steam HOME=/home/steam WLR_LIBINPUT_NO_DEVICES=1 \
     SDL_GAMECONTROLLERCONFIG="$SDL_GAMECONTROLLERCONFIG" \
     gamescope \
@@ -91,7 +88,7 @@ sudo -E -u steam HOME=/home/steam WLR_LIBINPUT_NO_DEVICES=1 \
     -F fsr \
     --force-grab-cursor \
     -- \
-    steam -gamepadui -tenfoot &
+    steam -gamepadui -pipewire-dmabuf &
 
 GS_PID=$!
 
