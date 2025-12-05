b239a4ae9789f13b7056e6107967fa18beb63b34
Gunnar <git@grundstil.de>
2025-12-05 20:22:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -132,8 +132,12 @@ chown -R steam:steam /home/steam/.steam/root/compatibilitytools.d
 # --- 7. Gamescope ---
 echo "Starting Gamescope..."
 
+# This string targets GUID 050000004c05... which is exclusively the PS5 controller on Linux.
+# It will NOT affect Xbox controllers (which start with 030000005e04...).
+export SDL_GAMECONTROLLERCONFIG="050000004c050000c405000000850000,PS5 Controller,a:b0,b:b1,back:b8,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b10,leftshoulder:b4,leftstick:b11,lefttrigger:a2,leftx:a0,lefty:a1,rightshoulder:b5,rightstick:b12,righttrigger:a5,rightx:a3,righty:a4,start:b9,x:b2,y:b3,platform:Linux,"
 sudo -E -u steam HOME=/home/steam WLR_LIBINPUT_NO_DEVICES=1 \
     UG_MAX_BUFFERS=256 \
+    SDL_GAMECONTROLLERCONFIG="$SDL_GAMECONTROLLERCONFIG" \
     gamescope \
     -W "$WIDTH" -H "$HEIGHT" \
     -w "$WIDTH" -h "$HEIGHT" \
